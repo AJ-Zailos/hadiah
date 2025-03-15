7836cc3a45d1af8490c563fa816e34d6d91062c6
AJ-Zailos <justin.aviel.wiryos@gmail.com>
2025-03-15 22:08:45 +0700

    Update wishj.js

--- wishj.js
+++ wishj.js
@@ -1,6 +1,9 @@
 function matchheight() {
     const img = document.getElementById("mele");
     const div = document.getElementById("all");
+    const vid = document.getElementById("videonya");
+    const eee = document.getElementById("bday");
+    vid.style.width = eee.clientWidth + "px";
     div.style.height = img.clientHeight + "px";
 }
 
